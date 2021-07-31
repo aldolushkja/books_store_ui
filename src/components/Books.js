5ffa02d955328d801b5736e256b3dea03c2bf6a9
Aldo Lushkja <a.lushkja@namirial.com>
2021-07-31 15:03:08 +0200

    ui refactoring on OneBook

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,9 +1,9 @@
-import React, { Component, Fragment } from "react";
-import { Link } from "react-router-dom";
+import React, {Component, Fragment} from "react";
+import {Link} from "react-router-dom";
 import Loading from "./../images/loading.gif";
 
 export default class Books extends Component {
-    state = { books: [], isLoaded: false, error: null };
+    state = {books: [], isLoaded: false, error: null};
 
     componentDidMount() {
         console.log("componentDidMount::Books");
@@ -25,33 +25,30 @@ export default class Books extends Component {
                         books: json,
                         isLoaded: true,
                     });
-                }, 2000);
+                }, 3000);
             });
     }
+
     render() {
-        const { books, isLoaded, error } = this.state;
+        const {books, isLoaded, error} = this.state;
         if (error) {
             return <div className="bg bg-danger">Error: {error.message}</div>;
         } else if (!isLoaded) {
-            return <img src={Loading} alt="loading..." width="300" height="300" />;
+            return <img src={Loading} alt="loading..." width="300" height="300"/>;
         } else {
             return (
                 <Fragment>
-                    <div className="card">
-                        <div className="card-body">
-                            <div className="card-title">All Books</div>
-                            <div className="card-text">
-                                {books.map((c) => (
-                                    <Link
-                                        key={c.id}
-                                        to={`/book/${c.id}`}
-                                        className="list-group-item list-group-item-action"
-                                    >
-                                        {c.title}{" "}
-                                    </Link>
-                                ))}
-                            </div>
-                        </div>
+                    <h2>Choose a book!</h2>
+                    <div className="list-group">
+                        {books.map((c) => (
+                            <Link
+                                key={c.id}
+                                to={`/book/${c.id}`}
+                                className="list-group-item list-group-item-action"
+                            >
+                                {c.title}{" "}
+                            </Link>
+                        ))}
                     </div>
                 </Fragment>
             );
--- src/components/OneBook.js
+++ src/components/OneBook.js
@@ -1,12 +1,13 @@
-import React, { Component, Fragment } from "react";
+import React, {Component, Fragment} from "react";
 import Loading from "./../images/loading.gif";
 
 export default class OneBook extends Component {
-    state = { book: {}, isLoaded: false, error: null };
-    
-    constructor(props){
+    state = {book: {}, isLoaded: false, error: null};
+
+    constructor(props) {
         super(props);
     }
+
     componentDidMount() {
         console.log("componentDidMount::OneBook");
         fetch("http://localhost:8080/api/v1/books/" + this.props.match.params.book_id)
@@ -22,32 +23,71 @@ export default class OneBook extends Component {
                 }
             })
             .then((json) => {
+                console.log(json)
                 setTimeout(() => {
                     this.setState({
                         book: json,
                         isLoaded: true,
                     });
-                }, 2000);
+                }, 3000);
             });
     }
+
     render() {
-        const { book, isLoaded, error } = this.state;
+        const {book, isLoaded, error} = this.state;
         if (error) {
             return <div className="bg bg-danger">Error: {error.message}</div>;
         } else if (!isLoaded) {
-            return <img src={Loading} alt="loading..." width="300" height="300" />;
+            return <img src={Loading} alt="loading..." width="300" height="300"/>;
         } else {
             return (
                 <Fragment>
-                    <div className="card">
-                        <div className="card-body">
-                            <div className="card-title">{book.title} - {book.publishedAt}</div>
-                            <div className="card-text">
-                                Author: {book.authors[0].name} <br />
-                                Genre: {book.genres[0].name}
-                            </div>
-                        </div>
+                    <h2>Book: {book.title}</h2>
+                    <div className="float-start">
+                        <small>Rating: </small>
+                        {Array(book.rating).fill(null).map((x, i) => (
+                            <i className="bi bi-star-fill" key={i}/>
+                        ))}
                     </div>
+                    <div className="float-end">
+                        {book.genres.map((m, index) => (
+                            <span className="badge bg-secondary me-1" key={index}>
+                    {m.name}
+                        </span>
+                        ))}
+                    </div>
+
+                    <div className="clearfix"/>
+                    <hr/>
+                    <table className="table table-compact table-striped">
+                        <thead></thead>
+                        <tbody>
+                        <tr>
+                            <td><strong>Publisher:</strong></td>
+                            <td>{book.publisher.name}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Published:</strong></td>
+                            <td>{book.publishedAt}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>ISBN:</strong></td>
+                            <td>{book.isbn}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Pages:</strong></td>
+                            <td>{book.pages}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Description:</strong></td>
+                            <td>{book.description}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Price:</strong></td>
+                            <td>{book.prices[0].value.toFixed(2)} {book.prices[0].symbol} </td>
+                        </tr>
+                        </tbody>
+                    </table>
                 </Fragment>
             );
         }
